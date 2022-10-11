2ec333cd0a689ff5c3dad2572141833899b68258
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2022-10-11 00:03:14 -0300

    Adiciona Delete Estado

--- 006-aplicacao/server/src/routes/estados.js
+++ 006-aplicacao/server/src/routes/estados.js
@@ -3,22 +3,19 @@ import { CreateEstadoController } from '../controllers/estados/CreateEstadoContr
 import { GetAllEstadosController } from '../controllers/estados/GetAllEstadosController.js';
 import { GetEstadoByIdController } from '../controllers/estados/GetEstadoByIdController.js';
 import { UpdateEstadoController } from '../controllers/estados/UpdateEstadoController.js';
+import { DeleteEstadoController } from '../controllers/estados/DeleteEstadoController.js';
 
 const estadoRouter = Router();
 const createEstadoController = new CreateEstadoController();
 const getAllEstadosController = new GetAllEstadosController();
 const getEstadoByIdController = new GetEstadoByIdController();
 const updateEstadoController = new UpdateEstadoController();
-
-estadoRouter.get('/', (request, response) => {
-    response.json({
-        message: "Server is ruuuuunning :)"
-    })
-});
+const deleteEstadoController = new DeleteEstadoController();
 
 estadoRouter.post('/estados', createEstadoController.handle);
 estadoRouter.get('/estados', getAllEstadosController.handle);
 estadoRouter.get('/estados/:id', getEstadoByIdController.handle);
 estadoRouter.put('/estados', updateEstadoController.handle);
+estadoRouter.delete('/estados', deleteEstadoController.handle);
 
 export { estadoRouter }
