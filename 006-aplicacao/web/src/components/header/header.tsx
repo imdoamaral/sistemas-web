a5018dfd05954efe2823fb08d680ce6b1bd04c72
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2022-10-12 21:12:37 -0300

    Cria projeto React e primeiro componente

---
+++ 006-aplicacao/web/src/components/header/header.tsx
@@ -0,0 +1,19 @@
+import './header.css';
+
+interface HeaderProps {
+    name: string
+}
+
+const Header = (props: HeaderProps) => {
+
+    return (
+        <header>
+            <div className='header'>
+                <h1>Sistema de Doação de Sangue</h1>
+                <h2>Usuário: {props.name}</h2>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
